0709fb3a45f09ef659483a4847a5395622a60632
David Caldwell <david@porkrind.org>
2023-01-28 14:17:55 -0800

    build.rs,Cargo.toml: Switch to the `cc` crate as it seems more robust
    It knows how to set up the basic compiler flags across disparate
    machines (-fPIC in particular), where the `make` crate just runs
    `make` and prays for the best.
    
    This ends up duplicating some basic parts of the Makefile and might
    need updating if the Makefile ever adds a new file, but we'll get over
    it.

--- build.rs
+++ build.rs
@@ -1,8 +1,5 @@
 // Copyright © 2023 David Caldwell <david@porkrind.org>
 
-extern crate make_cmd;
-use make_cmd::make;
-
 fn main() {
     let out_dir = std::path::PathBuf::from(std::env::var("OUT_DIR").expect("no OUT_DIR from cargo"));
 
@@ -10,26 +7,23 @@ fn main() {
         println!("cargo:rerun-if-changed={}", file);
     }
 
-    let tzdir = std::env::current_dir().expect("current_dir").join("tz");
-
-    let mut make = make();
-    make.arg("-f").arg(tzdir.join("Makefile"))
-        .arg("libtz.a")
-        .arg(format!("VPATH={}", tzdir.to_str().expect("Bad unicode in current directory")))
-        .arg(format!("CFLAGS={}", ["-Dgetenv=rust_getenv",    // Hack to make the tz C code use rust's getenv (so that it is locked properly)
-                                   "-DTHREAD_SAFE",           // Make tz protect shared globals with a mutex
-                                   "-DSTD_INSPIRED=1",        // Add posix2time_z() and time2posix_z().
-                                   "-Dtime_tz=int64_t",       // Force libtz to use a 64 bit time_t
-                                   "-DHAVE_TZNAME=0",         // Don't export variables--they're inherently racey
-                                   "-DUSG_COMPAT=0",          // " " "
-                                   "-DALTZONE=0",             // " " "
-                                  ].join(" ")))
-        .current_dir(&out_dir);
-    println!("command: {:?}", make);
-    match make.status().expect("Make failed").code().expect("Make crashed?") {
-        0 => {},
-        e => { panic!("Make exited with {}", e); },
-    }
+    cc::Build::new()
+        .file("tz/localtime.c")
+        .file("tz/asctime.c")
+        .file("tz/difftime.c")
+        .file("tz/strftime.c")
+        .include("tz")
+        .warnings(false) // some of the flags we set up below generate "unused reference" warnings.
+        .static_flag(true)
+        .define("TZDIR",         r#""/usr/share/zoneinfo""#)      // The default from tz/Makefile
+        .define("getenv",        "rust_getenv")                   // Hack to make the tz C code use rust's getenv (so that it is locked properly)
+        .define("THREAD_SAFE",   None)                            // Make tz protect shared globals with a mutex
+        .define("STD_INSPIRED",  "1")                             // Add posix2time_z() and time2posix_z().
+        .define("time_tz",       "int64_t")                       // Force libtz to use a 64 bit time_t
+        .define("HAVE_TZNAME",   "0")                             // Don't export variables--they're inherently racey
+        .define("USG_COMPAT",    "0")                             // " " "
+        .define("ALTZONE",       "0")                             // " " "
+        .compile("tz");
 
     println!("cargo:rustc-link-lib=tz");
     println!("cargo:rustc-link-search=native={}", out_dir.display());
